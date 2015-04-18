a4e0e4dd3e59d2eb7985e30c983cfb53d7d0f163
Rachel Brindle <you@subluminal.net>
2015-04-17 21:55:49 -0700

    Initial Commit

---
+++ Muon/Muon.h
@@ -0,0 +1,19 @@
+//
+//  Muon.h
+//  Muon
+//
+//  Created by Rachel Brindle on 4/17/15.
+//  Copyright (c) 2015 rachelbrindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Muon.
+FOUNDATION_EXPORT double MuonVersionNumber;
+
+//! Project version string for Muon.
+FOUNDATION_EXPORT const unsigned char MuonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Muon/PublicHeader.h>
+
+
