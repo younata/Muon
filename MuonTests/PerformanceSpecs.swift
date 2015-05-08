188e120d69e4d4af6f5dae35046ebaff4c77af22
Rachel Brindle <you@subluminal.net>
2015-05-07 21:56:49 -0700

    Add performance tests

---
+++ MuonTests/PerformanceSpecs.swift
@@ -0,0 +1,36 @@
+import XCTest
+import Muon
+
+func parserWithContentsOfFile(fileName: String) -> FeedParser {
+    let location = NSBundle(forClass: AtomPerformanceTest.self).pathForResource(fileName, ofType: nil)!
+    let contents = String(contentsOfFile: location, encoding: NSUTF8StringEncoding, error: nil)!
+    let parser = FeedParser(string: contents)
+    return parser
+}
+
+class RSS1PerformanceTest: XCTestCase {
+    func testPerformance() {
+        let parser = parserWithContentsOfFile("rss1_large.rss")
+        self.measureBlock {
+            parser.main()
+        }
+    }
+}
+
+class RSS2PerformanceTest: XCTestCase {
+    func testPerformance() {
+        let parser = parserWithContentsOfFile("rss2_large.rss")
+        self.measureBlock {
+            parser.main()
+        }
+    }
+}
+
+class AtomPerformanceTest: XCTestCase {
+    func testPerformance() {
+        let parser = parserWithContentsOfFile("atom_large.xml")
+        self.measureBlock {
+            parser.main()
+        }
+    }
+}
